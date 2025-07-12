c00924768e2214c84b9393df7ea1c1969701fab8
abdellatif000 <63450459+abdellatif000@users.noreply.github.com>
2025-07-12 16:29:27 +0000

    Initial commit

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,13 +1,6 @@
 import type { GlucoseEntry } from '@/lib/types';
 
 export const initialEntries: GlucoseEntry[] = [
-  {
-    id: '1',
-    date: new Date('2024-07-15T08:00:00'),
-    time: '08:00',
-    mealType: 'breakfast',
-    glycemia: 1.10,
-    novorapidDosage: 8,
-  },
+ 
   
 ];
