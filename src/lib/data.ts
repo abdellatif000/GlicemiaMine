a56369ee7918a5cff342588a12aadcc362bf9921
abdellatif000 <63450459+abdellatif000@users.noreply.github.com>
2025-07-12 13:26:41 +0000

    Initial commit

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -9,28 +9,5 @@ export const initialEntries: GlucoseEntry[] = [
     glycemia: 1.10,
     novorapidDosage: 8,
   },
-  {
-    id: '2',
-    date: new Date('2024-07-15T12:30:00'),
-    time: '12:30',
-    mealType: 'lunch',
-    glycemia: 1.45,
-    novorapidDosage: 10,
-  },
-  {
-    id: '3',
-    date: new Date('2024-07-15T19:00:00'),
-    time: '19:00',
-    mealType: 'dinner',
-    glycemia: 0.95,
-    novorapidDosage: 12,
-  },
-  {
-    id: '4',
-    date: new Date('2024-07-16T08:15:00'),
-    time: '08:15',
-    mealType: 'breakfast',
-    glycemia: 1.80,
-    novorapidDosage: 9,
-  },
+  
 ];
